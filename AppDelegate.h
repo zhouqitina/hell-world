8c422609b5876d5afb40abb210190dcd9bea28f2
zhouqitina <zhouqijiaoting@icloud.com>
2017-05-24 21:45:56 +0800

    Add files via upload

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  123edfr
+//
+//  Created by 周奇 on 2017/5/24.
+//  Copyright © 2017年 周奇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
